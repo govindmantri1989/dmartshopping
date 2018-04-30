d94161c2741c893f947e538cc8de90c7610aee54
govindmantri1989 <rknecbackup@gmail.com>
2018-04-30 08:07:24 +0530

    Dev-100 Commited by Govind Mantri This is First Commit , Just Set up Project with Maven

---
+++ dmartonlineshoppingbackend/src/main/java/com/universal/dmartonlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.universal.dmartonlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
